2d6902e9e981bb852e72db4fd7efe513abbbc286
Lilit Bandeira <devlilitbandeira@gmail.com>
2024-09-11 21:47:11 -0300

    trying port 8080

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 
 config();
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
